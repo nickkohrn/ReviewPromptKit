03b0396a6cf5ecd7a74a1a1b20ff3426d6a76a54
Nick Kohrn <bnkohrn@icloud.com>
2023-05-13 10:28:28 -0400

    Add package files

---
+++ README.md
@@ -0,0 +1,3 @@
+# ReviewPromptKit
+
+A description of this package.
---
+++ Sources/ReviewPromptKit/ReviewPromptKit.swift
@@ -0,0 +1,6 @@
+public struct ReviewPromptKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
